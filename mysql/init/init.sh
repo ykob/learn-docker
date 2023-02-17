54a1a2af38cd0d01d981d931695df309eaf700aa
ykob <info@tplh.net>
2023-02-18 08:43:16 +0900

    added init file.

---
+++ mysql/init/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create database
+mysql -u root -proot -e "DROP DATABASE IF EXISTS company; CREATE DATABASE company;"
+
+# create tables
+mysql -u root -proot company < "/docker-entrypoint-initdb.d/sql/create-tables.sql"
+
+# import data
+mysql -u root -proot company < "/docker-entrypoint-initdb.d/sql/data/prefecture.sql"
+mysql -u root -proot company < "/docker-entrypoint-initdb.d/sql/data/employee.sql"
+mysql -u root -proot company < "/docker-entrypoint-initdb.d/sql/data/employee_address.sql"
+mysql -u root -proot company < "/docker-entrypoint-initdb.d/sql/data/employee_salary.sql"
