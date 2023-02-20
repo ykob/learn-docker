41c77a7a8447a1f0360d73f37c97c39c23c9a7ae
ykob <info@tplh.net>
2023-02-20 21:14:58 +0900

    added employee.sql

---
+++ mysql/init/sql/data/employee.sql
@@ -0,0 +1,53 @@
+INSERT INTO employee
+SET number = 'DEV-0001',
+name = '佐藤 一郎',
+email = 'tanaka@sample.com',
+role = 'developer',
+position = 'manager',
+hire_date = '2020-01-01'
+;
+
+INSERT INTO employee
+SET number = 'DEV-0002',
+name = '鈴木 二郎',
+email = 'suzuki@sample.com',
+role = 'developer',
+position = 'chief',
+hire_date = '2020-03-01'
+;
+
+INSERT INTO employee
+SET number = 'DEV-0003',
+name = '高橋 三郎',
+email = 'takahashi@sample.com',
+role = 'developer',
+position = null,
+hire_date = '2020-05-01'
+;
+
+INSERT INTO employee
+SET number = 'SAL-0001',
+name = '田中 四郎',
+email = 'tanaka@sample.com',
+role = 'sales',
+position = 'manager',
+hire_date = '2020-02-01'
+;
+
+INSERT INTO employee
+SET number = 'SAL-0002',
+name = '渡辺 五郎',
+email = 'watanabe@sample.com',
+role = 'sales',
+position = 'chief',
+hire_date = '2020-04-01'
+;
+
+INSERT INTO employee
+SET number = 'SAL-0003',
+name = '伊藤 六郎',
+email = 'ito@sample.com',
+role = 'sales',
+position = null,
+hire_date = '2020-06-01'
+;
