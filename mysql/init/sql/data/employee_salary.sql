be86b80eddb829228b733c9d78341d0a46a8f955
ykob <info@tplh.net>
2023-02-22 20:52:34 +0900

    added INSERT INTO employee_salary.

--- mysql/init/sql/data/employee_salary.sql
+++ mysql/init/sql/data/employee_salary.sql
@@ -93,3 +93,147 @@ SET employee_id = (SELECT id FROM employee where number = 'DEV-0001')
 , is_bonus = FALSE
 , transfered_at = '2022-12-25T09:00:00'
 ;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202203'
+, payment_amount = 300000
+, is_bonus = FALSE
+, transfered_at = '2022-03-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202204'
+, payment_amount = 300000
+, is_bonus = FALSE
+, transfered_at = '2022-04-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202205'
+, payment_amount = 300000
+, is_bonus = FALSE
+, transfered_at = '2022-05-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202206'
+, payment_amount = 300000
+, is_bonus = FALSE
+, transfered_at = '2022-06-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202207'
+, payment_amount = 300000
+, is_bonus = FALSE
+, transfered_at = '2022-07-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202208'
+, payment_amount = 300000
+, is_bonus = FALSE
+, transfered_at = '2022-08-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202209'
+, payment_amount = 330000
+, is_bonus = FALSE
+, transfered_at = '2022-09-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202210'
+, payment_amount = 330000
+, is_bonus = FALSE
+, transfered_at = '2022-10-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202211'
+, payment_amount = 330000
+, is_bonus = FALSE
+, transfered_at = '2022-11-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0002')
+, target_year_month = '202212'
+, payment_amount = 330000
+, is_bonus = FALSE
+, transfered_at = '2022-12-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0003')
+, target_year_month = '202205'
+, payment_amount = 200000
+, is_bonus = FALSE
+, transfered_at = '2022-05-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0003')
+, target_year_month = '202206'
+, payment_amount = 200000
+, is_bonus = FALSE
+, transfered_at = '2022-06-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0003')
+, target_year_month = '202207'
+, payment_amount = 200000
+, is_bonus = FALSE
+, transfered_at = '2022-07-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0003')
+, target_year_month = '202208'
+, payment_amount = 200000
+, is_bonus = FALSE
+, transfered_at = '2022-08-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0003')
+, target_year_month = '202209'
+, payment_amount = 240000
+, is_bonus = FALSE
+, transfered_at = '2022-09-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0003')
+, target_year_month = '202210'
+, payment_amount = 240000
+, is_bonus = FALSE
+, transfered_at = '2022-10-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0003')
+, target_year_month = '202211'
+, payment_amount = 240000
+, is_bonus = FALSE
+, transfered_at = '2022-11-25T09:00:00'
+;
+
+INSERT INTO employee_salary
+SET employee_id = (SELECT id FROM employee where number = 'DEV-0003')
+, target_year_month = '202212'
+, payment_amount = 240000
+, is_bonus = FALSE
+, transfered_at = '2022-12-25T09:00:00'
+;
