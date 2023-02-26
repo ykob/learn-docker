afab924cb63fe352a2c5dbf2a936a035e453ea4e
ykob <info@tplh.net>
2023-02-26 22:28:21 +0900

    adjustted sql code.

--- testing-mysql-with-docker/init/sql/data/employee.sql
+++ testing-mysql-with-docker/init/sql/data/employee.sql
@@ -1,5 +1,5 @@
-INSERT INTO employee
-SET number = 'DEV-0001',
+INSERT INTO employee SET
+number = 'DEV-0001',
 name = '佐藤 一郎',
 email = 'sato@sample.com',
 role = 'developer',
@@ -7,8 +7,8 @@ position = 'manager',
 hire_date = '2022-01-01'
 ;
 
-INSERT INTO employee
-SET number = 'DEV-0002',
+INSERT INTO employee SET
+number = 'DEV-0002',
 name = '鈴木 二郎',
 email = 'suzuki@sample.com',
 role = 'developer',
@@ -16,8 +16,8 @@ position = 'chief',
 hire_date = '2022-03-01'
 ;
 
-INSERT INTO employee
-SET number = 'DEV-0003',
+INSERT INTO employee SET
+number = 'DEV-0003',
 name = '高橋 三郎',
 email = 'takahashi@sample.com',
 role = 'developer',
@@ -25,8 +25,8 @@ position = null,
 hire_date = '2022-05-01'
 ;
 
-INSERT INTO employee
-SET number = 'SAL-0001',
+INSERT INTO employee SET
+number = 'SAL-0001',
 name = '田中 四郎',
 email = 'tanaka@sample.com',
 role = 'sales',
@@ -34,8 +34,8 @@ position = 'manager',
 hire_date = '2022-02-01'
 ;
 
-INSERT INTO employee
-SET number = 'SAL-0002',
+INSERT INTO employee SET
+number = 'SAL-0002',
 name = '渡辺 五郎',
 email = 'watanabe@sample.com',
 role = 'sales',
@@ -43,8 +43,8 @@ position = 'chief',
 hire_date = '2022-04-01'
 ;
 
-INSERT INTO employee
-SET number = 'SAL-0003',
+INSERT INTO employee SET
+number = 'SAL-0003',
 name = '伊藤 六郎',
 email = 'ito@sample.com',
 role = 'sales',
