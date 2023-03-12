57f02584c4656ebc2eb4ecc663f57ed24523dc7a
ykob <info@tplh.net>
2023-03-12 11:54:23 +0900

    added to run update query on time api.

--- php-with-docker/php/src/time/index.php
+++ php-with-docker/php/src/time/index.php
@@ -1,8 +1,29 @@
 <?php
 header('Content-Type: application/json; charset=UTF-8');
 
-$arr['start'] = date('Y-m-d H:i:s');
-$arr['end'] = date('Y-m-d H:i:s', strtotime('+1 hour'));
+$mysqli = mysqli_connect('mysql', 'test', 'test', 'test');
+mysqli_select_db($mysqli, 'test');
+$data = mysqli_query($mysqli, 'SELECT * FROM time');
+
+$arr['current'] = date('Y-m-d H:i:s');
+$row = mysqli_fetch_assoc($data);
+$arr['data'] = $row;
+
+if ($row['start'] != null && $row['end'] != null) {
+  $stmt = mysqli_prepare($mysqli, 'UPDATE time SET current = ?');
+  mysqli_stmt_bind_param($stmt, 's', $arr['current']);
+  mysqli_stmt_execute($stmt);
+  $arr['start'] = $row['start'];
+  $arr['end'] = $row['end'];
+} else {
+  $start = date('Y-m-d H:i:s');
+  $end = date('Y-m-d H:i:s', strtotime('+1 hour'));
+  $stmt = mysqli_prepare($mysqli, 'UPDATE time SET start = ?, end = ?, current = ?');
+  mysqli_stmt_bind_param($stmt, 'sss', $start, $end, $arr['current']);
+  mysqli_stmt_execute($stmt);
+  $arr['start'] = $start;
+  $arr['end'] = $end;
+}
 
 print json_encode($arr, JSON_PRETTY_PRINT);
 ?>
